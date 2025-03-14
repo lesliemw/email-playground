fedfb2bcc3041990c5d0cfb810353fd5422d80ff
lesliemw <l.marie1598@gmail.com>
2025-03-14 20:54:16 +0000

    update file

---
+++ email_sender.py
@@ -0,0 +1,17 @@
+import smtplib
+
+from email.message import EmailMessage
+
+email = EmailMessage()
+email['from'] = 'Leslie Williams'
+email['to'] = 'l.marie1598@gmail.com'
+email['subject'] = 'You won 1,000,000 dollars!'
+email.set_content(
+    'I think you are amazing! You are the best person in the world! You have won 1,000,000 dollars! Click here to claim your prize!')
+
+with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp:
+    smtp.ehlo()
+    smtp.starttls()
+    smtp.login('l.marie1598@gmail.com', '')
+    smtp.send_message(email)
+    print('all good boss!')
