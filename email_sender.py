65a36eab02c14052d85292891fef475998b7aa0b
lesliemw <l.marie1598@gmail.com>
2025-03-14 21:03:29 +0000

    update file

--- email_sender.py
+++ email_sender.py
@@ -1,5 +1,4 @@
 import smtplib
-
 from email.message import EmailMessage
 from string import Template
 from pathlib import Path
@@ -9,12 +8,11 @@ email = EmailMessage()
 email['from'] = 'Leslie Williams'
 email['to'] = 'l.marie1598@gmail.com'
 email['subject'] = 'You won 1,000,000 dollars!'
-email.set_content(
-    'I think you are amazing! You are the best person in the world! You have won 1,000,000 dollars! Click here to claim your prize!')
+email.set_content(html.substitute({'name': 'Leslie'}), 'html')
 
 with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp:
     smtp.ehlo()
     smtp.starttls()
-    smtp.login('l.marie1598@gmail.com', '')
+    smtp.login('l.marie1598@gmail.com', 'password')
     smtp.send_message(email)
     print('all good boss!')
